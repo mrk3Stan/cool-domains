13cd1a1c2a024fa1aeb53e1b26e4194bd9cbefc7
Mark Bustillo <markbustillo@cryptoblk.io>
2022-08-22 16:02:14 +0800

    add hardhat config

---
+++ hardhat.config.js
@@ -0,0 +1,15 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-chai-matchers");
+require("@nomiclabs/hardhat-ethers");
+require("dotenv").config()
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.9",
+  networks: {
+    mumbai: {
+      url: process.env.ALCHEMY_MUMBAI_URL,
+      accounts: [process.env.PRIVATE_KEY],
+    }
+  }
+};
